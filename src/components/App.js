7e97e3673dc0b43365d58260709aa31913562b6c
K8Sewell <kait@kaitsewell.com>
2020-01-17 21:17:08 -0800

    updates Footer to ShowFilter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
+// imports library
 import React from 'react'
-import Footer from './Footer'
+
+// imports containers and components
 import AddTodo from '../containers/AddTodo'
+import ShowFilter from './ShowFilter'
 import VisibleTodoList from '../containers/VisibleTodoList'
+
+// functional component acting as main container for application
 const App = () => (
   <div>
     <AddTodo />
+    <ShowFilter />
     <VisibleTodoList />
-    <Footer />
   </div>
 )
+
+// exports app container
 export default App
--- src/components/ShowFilter.js
+++ src/components/ShowFilter.js
@@ -1,7 +1,12 @@
+// imports library
 import React from 'react'
+
+// imports container and action
 import FilterLink from '../containers/FilterLink'
 import { VisibilityFilters } from '../actions'
-const Footer = () => (
+
+// functional component using an action to control which to dos to display
+const ShowFilter = () => (
   <p>
     Show: <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
     {', '}
@@ -10,4 +15,6 @@ const Footer = () => (
     <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
   </p>
 )
-export default Footer
+
+// exports showFilter component
+export default ShowFilter
